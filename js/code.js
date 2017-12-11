d44169286120e04af181cdafd436c1f5d529918e
Calrowe <bjornadell@hotmail.com>
2017-12-11 06:03:57 +0100

    Added some pics and object

--- js/code.js
+++ js/code.js
@@ -7,11 +7,19 @@ var gameState;
       };
    }
 
-   function drawPage(){
-      var page = zombieFakker[ gameState.currentPage];
+   $("#reset").on("click", function(){
+       gameState = {
+           currentPage: 'beginning'
+       };
+       drawPage(zombieFakker);
+   })
+
+   function drawPage(bla){
+      var page = bla[ gameState.currentPage];
       $("#page h2").text(page.title);
       $("#page img").attr("src", page.img);
       $("#bread").html(page.bread);
+      $("#choice").text(page.choice);
       $(".GameMenu ul").empty();
       page.options.forEach(function(link, n){
          var linkElem = $("<li></li>").text(link.text).attr("data-optNbr", n);
@@ -24,11 +32,19 @@ var gameState;
       var page = zombieFakker[ gameState.currentPage];
       var optionData = page.options[optNbr];
       var target = optionData.to;
+      if (target === "boom"){
+          gameState.currentPage = "woodAxe";
+         drawPage(stuffs);
+      }
       gameState.currentPage = target;
-      drawPage();
+      drawPage(zombieFakker);
    });
 
-   drawPage();
+   function chooseStuffs(){
+
+   }
+
+   drawPage(zombieFakker);
 
 });
 
--- js/data.js
+++ js/data.js
@@ -5,6 +5,7 @@ var zombieFakker = {
         bread: "...you sit in your favourite chair at the cozy fireplace you just lit.<br>" +
             "Suddenly you hear wierd scratching noices on the walls. You go to a window to see<br>" +
             "who or what is making the sound. What then meets your eyes makes your blood run cold.<br>",
+        choice: "Make a choice",
         options: [{text:"Next", to:"next"},{text:"Skip", to:"rucksack"}]
     },
     next: {
@@ -17,13 +18,26 @@ var zombieFakker = {
             "flesheating zombies. It contaiges by blood. If you are not bitten head to...'. Location and<br>" +
             "instructions to find the different safe zones follows. You quickly start to pack your rucksack<br>"+
             "as you listens to the radio.",
+        choice: "Make a choice",
         options: [{text:"Next", to:"rucksack"}]
     },
     rucksack: {
         title: "Rucksack",
         img: "Images/Rucksack.jpg",
         bread: "Of all items you have gathered from the cabin only a few will fit the bag so choose wisely.",
-        options: [{text:"Knife", to:"drawPage()"},{text:"Rifle", to:"rifle"}
+        choice: "Make a choice",
+        options: [{text:"Next", to:"boom"}
         ]
+    }
+};
+
+var stuffs = {
+    woodAxe: {
+        title: "Wood axe",
+        img: "Images/WoodAxe.jpg",
+        bread: "An axe used for shopping wood but its exellent at shopping zombies too you bet.<br><br>" +
+            "+3 attack, +1 survival",
+        choice: "Do you want to add it to your backpack?",
+        options: [{text:"Yes", to:"yes"}]
     }
 };
